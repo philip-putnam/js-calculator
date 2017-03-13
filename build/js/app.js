8ed12e22b13b01a8ef0553518dd1b7c23b79641b
Jake Campa and Philip Putnam <jake@>
2017-03-13 14:42:35 -0700

    add weather api and function call-back with hidden api key

--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = 'a1164fec78d371dcd7af2ab694c7e989'
+
+},{}],2:[function(require,module,exports){
 function Calculator(skinName) {
   this.skin = skinName;
 }
@@ -21,7 +24,23 @@ Calculator.prototype.pingPong = function(goal) {
 
 exports.calculatorModule = Calculator;
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
+var apiKey = require('./../.env').apiKey;
+
+Weather = function(){
+}
+
+Weather.prototype.getWeather = function(city, displayFunction) {
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+    displayFunction(city, response.main.humidity);
+  }).fail(function(error) {
+    $('.showWeather').text(error.responseJSON.message);
+  });
+}
+
+exports.weatherModule = Weather;
+
+},{"./../.env":1}],4:[function(require,module,exports){
 var Calculator = require('./../js/pingpong.js').calculatorModule;
 
 $(document).ready(function() {
@@ -45,4 +64,24 @@ $(document).ready(function() {
   });
 });
 
-},{"./../js/pingpong.js":1}]},{},[2]);
+$(document).ready(function(){
+  $('#time').text(moment());
+});
+
+var Weather = require('./../js/weather.js').weatherModule;
+
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text('The humidity in ' + city + ' is ' + humidityData + '%');
+}
+
+$(document).ready(function() {
+  var currentWeatherObject = new Weather();
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    var humidity = currentWeatherObject.getWeather(city);
+    currentWeatherObject.getWeather(city, displayHumidity);
+  });
+});
+
+},{"./../js/pingpong.js":2,"./../js/weather.js":3}]},{},[4]);
